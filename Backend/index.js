60f623f3eb6357de84ae2fd48ac0caaad2addbb9
Ishank Nagpal <73271919+ishank1212@users.noreply.github.com>
2023-04-30 01:46:12 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import studentRouter from './routes/studentsPost.js';
+
+const app = express();
+app.use(express.json());
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/new", studentRouter);
+
+const PORT = process.env.PORT || 6001;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
