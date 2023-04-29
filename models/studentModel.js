4e42af769b16517a3633954e25ac118de136585e
Ishank Nagpal <73271919+ishank1212@users.noreply.github.com>
2023-04-30 01:36:43 +0530

    Add files via upload

---
+++ models/studentModel.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  emotionalStatus: String
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+export default Student;
